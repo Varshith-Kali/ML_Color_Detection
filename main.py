9cc79e969ba73c0500c0962f7d13ea1342cd255f
Varshith-Kali <pulivarshit@gmail.com>
2024-02-27 21:30:17 +0530

    add a cv2.imshow window that shows the selected image

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ index = ["color_name", "color", "hex", "R", "G", "B"]
 csv = pd.read_csv("./colors_data.csv", names=index, header=None)
 
 cv2.namedWindow('image')
-cv2.setMouseCallback('image', draw_function)
+cv2.setMouseCallback('image', draw_function) 
+
+## Error in draw_function...
+## Unable to recognize or undefined error
 
 def draw_function(event, x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
@@ -39,3 +42,28 @@ def getColorName(R,G,B):
             minimum = d
             cname = csv.loc[i,"color_name"]
     return cname
+
+
+
+while(1):
+    cv2.imshow("image",img)
+    if (clicked):
+        #cv2.rectangle(image, startpoint, endpoint, color, thickness) -1 thickness fills rectangle entirely
+        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+
+        #Creating text string to display ( Color name and RGB values )
+        text = getColorName(r,g,b) + ' R='+ str(r) + ' G='+ str(g) + ' B='+ str(b)
+
+        #cv2.putText(img,text,start,font(0-7), fontScale, color, thickness, lineType, (optional bottomLeft bool) )
+        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+        #For very light colours we will display text in black colour
+        if(r+g+b>=600):
+            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+
+        clicked=False
+
+    #Break the loop when user hits 'esc' key 
+    if cv2.waitKey(20) & 0xFF ==27:
+        break
+
+cv2.destroyAllWindows()
